ab6a77b39ea56d1a0a00628476e96d8e98e831a9
Ohta <OhtaPano@vivaldi.net>
2021-10-14 12:52:43 +0900

    schedule bar create

--- src/components/ScheduleList.tsx
+++ src/components/ScheduleList.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export const ScheduleList: React.FC = () => {
   return (
     <div>
-      <p>
-        <Link to="/schedule">シフト予約</Link>
-      </p>
+      <p></p>
     </div>
   );
 };
